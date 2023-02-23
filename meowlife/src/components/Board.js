01fc564c8a82513421080e6fe2fbb2371c6630bb
khans8890 <kansumaira@gmail.com>
2023-02-23 11:33:58 -0500

    feat: logic of updating points + hearts

--- meowlife/src/components/Board.js
+++ meowlife/src/components/Board.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 // creating the board using h + v axis
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const verticalAxis = ['1', '2','3','4','5','6','7','8'];
+
 // arr to store the sprites to then be rendered 
  class Piece {
     constructor(image, x, y){
@@ -16,8 +17,6 @@ const verticalAxis = ['1', '2','3','4','5','6','7','8'];
 
 const pieces = [new Piece('assets/imgs/cat-bowl.png', 0, 2), new Piece('assets/imgs/cats-water-bowl.png', 0, 4), new Piece('assets/imgs/output-onlineimagetools.png', 0, 7), new Piece('assets/imgs/output-onlineimagetools.png', 1, 7), new Piece('assets/imgs/output-onlineimagetools.png', 2, 7), new Piece('assets/imgs/output-onlinepngtools.png', 5, 4)];
 
-//pieces.push({image: '', x: 0, y: 6})
-//console.log(pieces)
 
 export default function Board(){
     // arr to store tiles of board
@@ -34,44 +33,30 @@ export default function Board(){
         let interval = Math.floor(Math.random() * 100) + 1
         // here is where the hungerLvl is being dynamically updated
         updateHunger(interval)
-        // if hungerLvl is < 50, don't feed the cat and so the button should be unclickable
-        if(hungerLvl < 50){
-           // console.log('not hungry yet', hungerLvl)
-            // else, meow should go off, button should be clickable, then track users success or failure in objective
-        } else{
-          //  console.log('hungies:', hungerLvl)
-        }
-    }, 3000)
-    return () => clearInterval(hungerLvlTimer)
-}, [hungerLvl])
+        }, 3000)
+        return () => clearInterval(hungerLvlTimer)
+    }, [hungerLvl])
 
-useEffect(() => {
-    const thirstLvlTimer = setInterval(() => {
-        let interval = Math.floor(Math.random() * 100) + 1
-        updateThirst(interval)
-        if(thirstLvl < 50){
-           // console.log('not thirsty yet', thirstLvl)
-        } else{
-          //  console.log('thirsty:', thirstLvl)
-        }
-    }, 5000)
-    return () => clearInterval(thirstLvlTimer)
-}, [thirstLvl])
+    useEffect(() => {
+        const thirstLvlTimer = setInterval(() => {
+            let interval = Math.floor(Math.random() * 100) + 1
+            updateThirst(interval)
+        }, 5000)
+        return () => clearInterval(thirstLvlTimer)
+    }, [thirstLvl])
 
 // creating the x and y axis tiles, starting from left to right as JS renders them like this
   for(let y  = verticalAxis.length-1; y >= 0; y--){
    for(let x = 0; x < horizontalAxis.length; x++){
        const number = y + x + 2;
-       //console.log(y, x)
         let image = undefined;
 
         pieces.forEach((p) => {
-            //console.log(p.image)
             if(p.x === x && p.y === y){
                 image = p.image;
             }
         })
-       // tracking the number to then manipulate on the UI and giving it an id of its coordinates on the board
+       // rendering tiles based on their positioning (i.e. blue v pink) & passing in necessary props
             board.push(<Tiles image={image} hungerLvl={hungerLvl} updateHunger={updateHunger} thirstLvl={thirstLvl} updateThirst={updateThirst} userPoints={userPoints} updatePoints={updatePoints} catHearts={catHearts} updateHearts={updateHearts} number={number} x={x} y={y}/>)
    }
   }
--- meowlife/src/components/Tiles.js
+++ meowlife/src/components/Tiles.js
@@ -1,28 +1,50 @@
 import './Tiles.css'
 
-export default function Tiles({ image, catHearts, updateHearts, userPoints, updatePoints,hungerLvl, updateHunger, thirstLvl, updateThirst, number, x, y }){
+export default function Tiles({ image, catHearts, updateHearts, userPoints, updatePoints, hungerLvl, updateHunger, thirstLvl, updateThirst, number, x, y }){
+
+    const handleSpriteClick = (e) => {
+        // verifying the sprite clicked + hunger lvl is above 50
+            if(e.target.id === "4" && hungerLvl > 50){
+                updatePoints(userPoints += 100)
+                updateHunger(0)
+                console.log('hunger', hungerLvl, userPoints)
+                // checking if game redeem cat hearts if necessary (i.e. they lost hearts but now gained 1)
+                if(catHearts < 3){
+                    updateHearts(catHearts += 1)
+                    console.log(catHearts)
+                }
+        // verifying the sprite clicked + thirst lvl is above 50
+            } else if(e.target.id === "6" && thirstLvl > 50){
+                updatePoints(userPoints += 100)
+                updateThirst(0)
+                console.log('thirst', thirstLvl, userPoints)
+            // checking if game redeem cat hearts if necessary (i.e. they lost hearts but now gained 1)
+                if(catHearts < 3){
+                        updateHearts(catHearts += 1)
+                        console.log(catHearts)
+                    }
+        // checking if the wrong sprite was clicked
+            } else if (e.target.id === "4" && hungerLvl <= 50){
+                updatePoints(userPoints -= 50)
+                updateHearts(catHearts -= 1)
+                console.log(userPoints, catHearts)
+            } else if (e.target.id === "6" && thirstLvl <= 50){
+                updatePoints(userPoints -= 50)
+                updateHearts(catHearts -= 1)
+                console.log(userPoints, catHearts)
+            }
+          }
+
     if(number % 2 === 0){
             return (
                 <div className='tile pink-tile'>
-                    <img src={image} style={{width: '85px'}} onClick={() => {
-                if(hungerLvl > 50){
-                    console.log('fed', hungerLvl)
-                    updatePoints(userPoints += 100)
-                    updateHunger(0)
-                } else if(thirstLvl > 50){
-                    console.log('drank', thirstLvl)
-                    updatePoints(userPoints += 100)
-                    updateThirst(0)
-                }
-              }}>
-            </img>
+                    <img id={number} src={image} style={{width: '85px'}} onClick={(e) => handleSpriteClick(e)}></img>
                 </div>
             )
         } else {
             return (
             <div className = 'tile blue-tile'>
-                <img src={image} style={{width: '85px'}}>
-            </img>
+                <img id={number} src={image} style={{width: '85px'}} onClick={(e) => handleSpriteClick(e)}></img>
             </div>
             ) 
         }
